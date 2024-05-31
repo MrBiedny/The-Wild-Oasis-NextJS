f0bd1042651948041cd0b025dbad63497ab367fb
MrBiedny <MrBiedny@gmail.com>
2024-06-01 00:03:11 +0200

    initial commit

---
+++ app/_components/SignOutButton.js
@@ -0,0 +1,15 @@
+import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/solid";
+import { singOutAction } from "../_lib/actions";
+
+function SignOutButton() {
+  return (
+    <form action={singOutAction}>
+      <button className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full">
+        <ArrowLeftStartOnRectangleIcon className="h-5 w-5 text-primary-600" />
+        <span>Sign out</span>
+      </button>
+    </form>
+  );
+}
+
+export default SignOutButton;
